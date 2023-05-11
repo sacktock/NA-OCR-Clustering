1804e3335a9ca812b47a19d11dd9256722cfedae
sacktock <alex.w.goodall@gmail.com>
2023-05-11 14:47:55 +0100

    update README ; upload OCR text

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ This is a proof of concept implementation of a document clustering pipeline. Our
 
 ## Idea
 
-The main idea is to extract text with the best off-the-shelf optical character recognition (OCR) tools. Then apply a spell checking tool on the extrated text to create a one-hot vector of mistakes. Clustering with k-means (or similar) on these one-hot vectors should cluster documents with similar mistakes and we hypothesise from similar eras or by similar scribes/wiritng styles.
+The main idea is to extract text with the best off-the-shelf optical character recognition (OCR) tools. Then apply a spell checking tool on the extrated text to create a one-hot vector for each of the mistakes. Clustering with k-means (or similar) on multi-hot vector encodings of the documents should cluster documents with similar mistakes and we hypothesise from similar eras or by similar scribes/wiritng styles.
 
 ## Proposed Pipeline
 
-Apply OCR -> Clean up extracted text -> spell check text document -> create one-hot vector encodings for each of the documents (using most common spelling mistakes in the dataset) -> k-means clustering on the dataset -> visualisation tools or run batch scripts on the clusters
+Apply OCR -> Clean up extracted text -> spell check text document -> create one-hot vector encodings for each of the mistakes (using most common spelling mistakes in the dataset) -> cerate multi-hot vector for each document -> k-means clustering on the dataset -> visualisation tools or run batch scripts on the clusters
  
 ## Dataset
 
--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import os
 from collections import defaultdict
 import numpy as np
 import heapq
+from sklearn import metrics
+from time import time
 
 DIMENSION = 1000
 
@@ -13,33 +15,33 @@ if __name__ == '__main__':
     if not os.path.exists(tmp_dir):
         os.makedirs(tmp_dir)
 
-    filenames = [filename for filename in os.listdir(dataset_dir) if filename.endswith(".pdf")]
+    filenames = ([filename for filename in os.listdir(dataset_dir) if filename.endswith(".pdf")]).sort()
 
     for filename in filenames:
         filepath = os.path.join(dataset_dir, filename)
         outpath = os.path.join(tmp_dir, filename[:-4]+"-OCR.txt")
         # Run OCR
-        if not os.path.exists(outpath)
+        if not os.path.exists(outpath):
             exit_code = os.system(f'ocrmypdf --sidecar {outpath} --force-ocr --deskew --clean --output-type=none {filepath} - > sdout.txt')
-            if exit_code = 0:
+            if exit_code == 0:
                 print(f'OCR {filename} successful')
             else:
                 print(f'OCR {filename} unsuccessful')
         # Run clean script
         inpath = os.path.join(tmp_dir, filename[:-4]+"-OCR.txt")
         outpath = os.path.join(tmp_dir, filename[:-4]+"-OCR-clean.txt")
-        if not os.path.exists(outpath)
+        if not os.path.exists(outpath):
             exit_code = os.system(f'python clean.py -i {inpath} -o {outpath}')
-            if exit_code = 0:
+            if exit_code == 0:
                 print(f'Cleaned {filename} successfuly')
             else:
                 print(f'Cleaned {filename} unsuccessfuly')
         # Run spell check
         inpath = os.path.join(tmp_dir, filename[:-4]+"-OCR-clean.txt")
         outpath = os.path.join(tmp_dir, filename[:-4]+"-OCR-mistakes.txt")
-        if not os.path.exists(outpath)
+        if not os.path.exists(outpath):
             exit_code = os.system(f'aspell --lang=en --mode=none --list < {inpath} > {outpath}')
-            if exit_code = 0:
+            if exit_code == 0:
                 print(f'Spell checked {filename} successfuly')
             else:
                 print(f'Spell checked {filename} unsuccessfuly')
@@ -73,6 +75,53 @@ if __name__ == '__main__':
 
     # TODO clustering & visualisation
 
+    # Create dataset labels 
+    label_dict = {'3': 0, '57':1, '885':2, '2248':3}
+    labels = {filename : label_dict[filename.split('-')[2]] for filename in filenames}
+
+    evaluations = []
+    evaluations_std = []
+
+
+    def fit_and_evaluate(km, X, name=None, n_runs=5):
+        name = km.__class__.__name__ if name is None else name
+
+        train_times = []
+        scores = defaultdict(list)
+        for seed in range(n_runs):
+            km.set_params(random_state=seed)
+            t0 = time()
+            km.fit(X)
+            train_times.append(time() - t0)
+            scores["Homogeneity"].append(metrics.homogeneity_score(labels, km.labels_))
+            scores["Completeness"].append(metrics.completeness_score(labels, km.labels_))
+            scores["V-measure"].append(metrics.v_measure_score(labels, km.labels_))
+            scores["Adjusted Rand-Index"].append(
+                metrics.adjusted_rand_score(labels, km.labels_)
+            )
+            scores["Silhouette Coefficient"].append(
+                metrics.silhouette_score(X, km.labels_, sample_size=2000)
+            )
+        train_times = np.asarray(train_times)
+
+        print(f"clustering done in {train_times.mean():.2f} ± {train_times.std():.2f} s ")
+        evaluation = {
+            "estimator": name,
+            "train_time": train_times.mean(),
+        }
+        evaluation_std = {
+            "estimator": name,
+            "train_time": train_times.std(),
+        }
+        for score_name, score_values in scores.items():
+            mean_score, std_score = np.mean(score_values), np.std(score_values)
+            print(f"{score_name}: {mean_score:.3f} ± {std_score:.3f}")
+            evaluation[score_name] = mean_score
+            evaluation_std[score_name] = std_score
+        evaluations.append(evaluation)
+        evaluations_std.append(evaluation_std)
+
+    
 
 
 
